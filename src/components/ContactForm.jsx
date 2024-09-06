64d1b979d3a42e3e457cbc31372aa3c8a7d59d73
Marco Antonio Juárez araoz <marcojuarezaraoz@gmail.com>
2024-09-05 23:50:52 -0600

    change the local DB for an AWS EC2 instance

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -46,7 +46,7 @@ const ContactForm = () => {
     e.preventDefault();
     if (validate()) {
       try {
-        const response = await fetch('http://18.205.103.247:3001/registers', {  // Cambiar localhost por la IP pública
+        const response = await fetch('/registers', {  // Cambiar localhost por la IP pública
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',

8a8c89d77090ec39d0c06d979977dc54469a65c9
MarcoJuarezVLIM <marco.juarez@vlim.com.mx>
2024-09-06 18:10:07 -0600

    add security constants

---
+++ backend/config.js
@@ -0,0 +1,9 @@
+export const FRONTHOST = process.env.FRONTHOST || 'http://localhost:5173';
+export const BACKHOST = 'http://localhost:3001';
+export const API = 'http://localhost:3001/registers';
+
+export const DB_HOST = process.env.DB_HOST || '18.205.103.247';
+export const DB_USER = 'postgres';
+export const DB_PASSWORD = 'marcoa';
+export const DB_DATABASE = 'emailPrueba';
+export const DB_PORT = '5432';
